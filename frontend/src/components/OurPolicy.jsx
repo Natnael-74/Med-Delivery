1fa63bbed98d4324b339309b401a152c7a8d934a
Natnael Endale <n87294578@gmail.com>
2026-03-15 23:23:51 +0300

    edit the policy part

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -9,7 +9,7 @@ function OurPolicy() {
       <div className="text-center py-8 text-3xl">
         <Title title="OUR" subTitle="POLICY" />
         <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700">
-          <div className="hover:shadow-md p-10 transition-all duration-300  w-1/4 ">
+          <div className="hover:shadow-md p-10 transition-all duration-300  md:w-1/4 ">
             <RiExchangeLine className="text-3xl w-12 m-auto mb-5" />
             <p className="font-semibold text-xl mb-3">Easy Exchange Policy</p>
             <p className="text-gray-400">
@@ -17,7 +17,7 @@ function OurPolicy() {
             </p>
           </div>
 
-          <div className="hover:shadow-md p-10 transition-all duration-300  w-1/4 ">
+          <div className="hover:shadow-md p-10 transition-all duration-300  md:w-1/4 ">
             <PiNumberCircleSeven className="text-3xl w-12 m-auto mb-5" />
             <p className="font-semibold text-xl mb-3">7 Days Money Back </p>
             <p className="text-gray-400">
@@ -25,7 +25,7 @@ function OurPolicy() {
             </p>
           </div>
 
-          <div className="hover:shadow-md p-10 transition-all duration-300  w-1/4 ">
+          <div className="hover:shadow-md p-10 transition-all duration-300  md:w-1/4 ">
             <RiCustomerServiceFill className="text-3xl w-12 m-auto mb-5" />
             <p className="font-semibold text-xl mb-3">Best Customer Service</p>
             <p className="text-gray-400">
