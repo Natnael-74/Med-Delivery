cb1545b148449c6fd3604825d1bc2a95207e5b5c
Natnael Endale <n87294578@gmail.com>
2026-03-16 01:04:10 +0300

    add the cart to context

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,15 +1,54 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, use, useContext, useEffect, useState } from "react";
 import products from "../assets/asset";
+import { toast } from "react-toastify";
 
 const ShopContext = createContext();
 
 function ShopProvider({ children }) {
-  const [serach, setSearch] = useState("");
+  const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
+  const [cart, setCart] = useState({});
 
   const currency = "$";
   const deliveryFee = 10;
   const productData = products;
+
+  function addToCart(productId, size) {
+    if (size === "") {
+      toast.error("Please select a size");
+      return;
+    }
+
+    const cartData = structuredClone(cart);
+    if (cartData[productId]) {
+      if (cartData[productId][size]) {
+        cartData[productId][size] += 1;
+      } else {
+        cartData[productId][size] = 1;
+      }
+    } else {
+      cartData[productId] = {};
+      cartData[productId][size] = 1;
+    }
+    setCart(cartData);
+    toast.success("Product added to cart");
+  }
+
+  function getCartCount() {
+    let count = 0;
+    for (const items in cart) {
+      for (const item in cart[items]) {
+        try {
+          if (cart[items][item] > 0) {
+            count += cart[items][item];
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+    return count;
+  }
 
   return (
     <ShopContext.Provider
@@ -17,10 +56,13 @@ function ShopProvider({ children }) {
         productData,
         currency,
         deliveryFee,
-        serach,
+        search,
         setSearch,
         showSearch,
         setShowSearch,
+        cart,
+        addToCart,
+        getCartCount,
       }}
     >
       {children}
