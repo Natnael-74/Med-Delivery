8f741ec20577bebb2b40e8fbdd96c70f03065b6e
Natnael Endale <n87294578@gmail.com>
2026-03-16 16:48:45 +0300

    the total to cart

---
+++ frontend/src/components/CartTotal.jsx
@@ -0,0 +1,38 @@
+import { useShop } from "../context/ShopContext";
+
+function CartTotal() {
+  const { currency, deliveryFee, getCartAmount } = useShop();
+  return (
+    <div className="w-full">
+      <div className="text-2xl">
+        <Title title="CART" subTitle="TOTAL" />
+      </div>
+      <div className="flex flex-col gap-2 mt-2 text-sm">
+        <div className="flex justify-between">
+          <p>Subtotal</p>
+          <p>
+            {currency}
+            {getCartAmount()}.00
+          </p>
+        </div>
+        <hr />
+        <div className="flex justify-between">
+          <p>Shipping Fee</p>
+          <p>
+            {currency}
+            {deliveryFee}.00
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <b>Total</b>
+          <b>
+            {currency}
+            {getCartAmount() === 0 ? 0 : getCartAmount + deliveryFee}.00
+          </b>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartTotal;
