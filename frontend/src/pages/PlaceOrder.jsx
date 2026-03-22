4afb5f078aeb1acda1e3bf1b96e98f49566f553c
Natnael Endale <n87294578@gmail.com>
2026-03-22 13:26:51 +0300

    add the place order page

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,5 +1,68 @@
+import Title from "../components/Title";
+
 function PlaceOrder() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-top">
+      {/* {left-side} */}
+
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm-text-2xl my-3">
+          <Title title="DELIVERY" subTitle="INFORMATION" />
+        </div>
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <input
+          type="email"
+          placeholder="Email"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+        <input
+          type="text"
+          placeholder="Street Address"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="City"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="State"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input
+            type="number"
+            placeholder="Zipcode"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Country"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <input
+          type="phone"
+          placeholder="Phone"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default PlaceOrder;
