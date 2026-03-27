37a200bcf598f966f7ca27bc06e77d421729e50d
Natnael Endale <n87294578@gmail.com>
2026-03-27 23:53:41 +0300

    add the messages to contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Title from "../components/Title";
 
 function Contact() {
   const [email, setEmail] = useState("");
@@ -7,7 +8,19 @@ function Contact() {
   function handleSubmit(e) {
     e.preventDefault();
   }
-  return <div>hhj</div>;
+  return (
+    <div>
+      <div className="text-center text-2xl border-t pt-10">
+        <Title title="CONTACT" subTitle="US" />
+      </div>
+      <div className="flex flex-col my-10 justify-center gap-10 md:flex-row mb-28">
+        <img className="w-full md:max-w-[480px]" src="" alt="" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our store</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
