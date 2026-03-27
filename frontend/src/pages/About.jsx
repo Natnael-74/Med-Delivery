a5ee3b63648cc1c44d1656dd2359f26ede1a55ec
Natnael Endale <n87294578@gmail.com>
2026-03-27 23:42:58 +0300

    add the correct messages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -31,7 +31,29 @@ function About() {
       <div className="text-xl py-4">
         <Title title="WHY" subTitle="CHOOSE US" />
       </div>
-      <div className="flex flex-col md:flex-row text-sm mb-20 "></div>
+      <div className="flex flex-col md:flex-row text-sm mb-20 ">
+        <div className="border px-10 md:px-14 py-8 sm:py-20 flex flex-col gap-5">
+          <b>QUality Assurance :</b>
+          <p className="text-gray-600">
+            we meticously check the quality of our products and services to
+            ensure it meets our standards.
+          </p>
+        </div>
+        <div className="border px-10 md:px-14 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convience :</b>
+          <p className="text-gray-600">
+            With our friendly and knowledgeable customer service team, you can
+            expect a smooth and hassle-free experience.
+          </p>
+        </div>
+        <div className="border px-10 md:px-14 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional customer service :</b>
+          <p className="text-gray-600">
+            Our team of dedicated professionals are here to help you every step
+            of the way, ensuring a seamless shopping experience.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
