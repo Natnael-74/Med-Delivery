5657fc14799d06644f797a36b9c2a9434cf9c432
Natnael Endale <n87294578@gmail.com>
2026-03-28 20:06:36 +0300

    add the router

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
 
+import Navbar from "./components/Navbar.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <div>
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+          </div>
+        </>
+      </div>
+    </div>
+  );
 }
 
 export default App;
