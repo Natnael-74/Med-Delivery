5e59f03e8d1087f09c08f0a1785aace22c4e4e0a
Natnael Endale <n87294578@gmail.com>
2026-03-28 18:39:59 +0300

    add server config for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    //always in the port 5173
+    port: 5173,
+  },
 });
