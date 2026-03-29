7d9d93a3b35930f145d01eebd85605527181191d
Natnael Endale <n87294578@gmail.com>
2026-03-29 12:05:21 +0300

    style the navbar

--- admin/admin/src/components/Navbar.jsx
+++ admin/admin/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import Logo from "./Logo";
 
-function Navbar() {
+function Navbar({ setToken }) {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
       <Logo />
-      <button className="bg-gray-600 text-white px-5 py-2 md:px-7 sm:py-2 rounded-full text-xs sm:text-sm hover:bg-gray-700 transition-all duration-300 cursor-pointer">
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 md:px-7 sm:py-2 rounded-full text-xs sm:text-sm hover:bg-gray-700 transition-all duration-300 cursor-pointer"
+      >
         LOGOUT
       </button>
     </div>
